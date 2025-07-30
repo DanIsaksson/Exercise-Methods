effa79d2f3faaa6e4fedaa2c7a5905179b964c93
Dan Isaksson <danisacsson@gmail.com>
2025-07-30 12:18:30 +0200

    First functioning version. Had a go at TryParse and Parse, gonna' have to do some exercises for the while(!int.TryParse(Console.ReadLine(), out inputC)) though. Not fully clear on how that works.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,9 +1,28 @@
 ﻿Console.WriteLine("Write three whole numerical values to calculate the median value: ");
-int inputA = Console.ReadLine();
-int inputB = Console.ReadLine();
-int inputC = Console.ReadLine();
+int inputA;
+while(!int.TryParse(Console.ReadLine(), out inputA))
+{
+     Console.WriteLine("That's... not a number... you can do this. I believe in you, try again: ");
+}
+int inputB;
+while(!int.TryParse(Console.ReadLine(), out inputB))
+{
+     Console.WriteLine("That's... not a number... you can do this. I believe in you, try again: ");
+}
+int inputC;
+while(!int.TryParse(Console.ReadLine(), out inputC))
+{
+     Console.WriteLine("That's... not a number... you can do this. I believe in you, try again: ");
+}
+
 
 MedianReturn(inputA, inputB, inputC);
+int result = MedianReturn(inputA, inputB, inputC);
+Console.WriteLine("\nYou have chosen the numbers " + inputA + ", " + inputB + " and " + inputC + ".");   
+Console.WriteLine("\nThe median is: " + result);
+
+// While loop to get it right
+
 
 int MedianReturn(int a, int b, int c)
 {
@@ -11,7 +30,6 @@ int MedianReturn(int a, int b, int c)
      b = inputB;
      c = inputC;
 
-     Console.WriteLine("You have chosen the numbers " + a + ", " + b + "and" + c + ".");   
      int median = (a+b+c)/3;
      return median;  
 }

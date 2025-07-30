ee5aaec56f20c0fde39faca9883fd15d70b75624
Dan Isaksson <danisacsson@gmail.com>
2025-07-30 11:44:47 +0200

    Trying to get this to work wondering if I needto use TryParse somehow.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write three whole numerical values to calculate the median value: ");
+int inputA = Console.ReadLine();
+int inputB = Console.ReadLine();
+int inputC = Console.ReadLine();
+
+MedianReturn(inputA, inputB, inputC);
+
+int MedianReturn(int a, int b, int c)
+{
+     a = inputA;
+     b = inputB;
+     c = inputC;
+
+     Console.WriteLine("You have chosen the numbers " + a + ", " + b + "and" + c + ".");   
+     int median = (a+b+c)/3;
+     return median;  
+}
